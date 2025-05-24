20487706c8457b88069a742611f9620c025e1270
Mukund Patil <mukundpatil5383@gmail.com>
2025-05-24 08:42:49 +0530

    Time: 3 ms (26.74%) | Memory: 33.7 MB (12.10%) - LeetSync

--- 3194-find-words-containing-character/find-words-containing-character.cpp
+++ 3194-find-words-containing-character/find-words-containing-character.cpp
@@ -1,27 +1,21 @@
 class Solution {
-private :
-    bool IsXpresent(char x, string word){
-        
-        int index =  word.find(x);
-        if(index != string::npos )
-        return true;
-        else  return false;
-        
-    }
 public:
     vector<int> findWordsContaining(vector<string>& words, char x) {
-        vector<int> XIndices;
-        
-        for(int i=0; i<words.size(); i++){
-            string word = words[i];
-           
-            if(IsXpresent(x,word)){
-            XIndices.push_back(i);
+        vector<int> Xindices;
+         int i =0;
+        for(string word:words){
+            for(char ch : word)
+            {
+                if(ch==x)
+                {
+                    Xindices.push_back(i);
+                    break;
+                }
             }
-            
+            i++;
         }
 
-        return XIndices;
+        return Xindices;
 
     }
 };
