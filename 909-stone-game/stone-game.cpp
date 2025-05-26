a71807dd31875c1004ceebefd12ded0563d67203
Mukund Patil <mukundpatil5383@gmail.com>
2025-05-26 22:33:20 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (89.10%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,6 +1,32 @@
 class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
-        return true;
+
+        int begin = 0, end = piles.size() - 1;
+        int alicePoints = 0, bobPoints = 0;
+        int TurnCounter = 0;
+
+        while (begin < end) {
+            if (TurnCounter % 2 == 0) {
+                if (piles[begin] < piles[end]) {
+                    alicePoints += piles[end];
+                    end--;
+                } else
+                    alicePoints += piles[begin];
+                begin++;
+            } else {
+                if (piles[begin] < piles[end]) {
+                    bobPoints += piles[end];
+                    end--;
+                } else
+                    bobPoints += piles[begin];
+                begin++;
+            }
+        }
+
+        if (bobPoints < alicePoints) {
+            return true;
+        } else
+            return false;
     }
 };
