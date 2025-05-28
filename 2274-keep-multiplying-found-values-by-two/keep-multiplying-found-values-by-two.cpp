c1f838f4ad5a5b2a34f6cacc7f58e1162cc72333
Mukund Patil <mukundpatil5383@gmail.com>
2025-05-28 22:39:23 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (99.79%) - LeetSync

--- 2274-keep-multiplying-found-values-by-two/keep-multiplying-found-values-by-two.cpp
+++ 2274-keep-multiplying-found-values-by-two/keep-multiplying-found-values-by-two.cpp
@@ -3,6 +3,7 @@ public:
     int findFinalValue(vector<int>& nums, int original) {
         
         sort(begin(nums),end(nums));
+        //sort the array nums then look for the element if found multiply the current orignal vallue  by 2
 
         for(int i =0;i<nums.size(); i++){
             if(nums[i]==original){
