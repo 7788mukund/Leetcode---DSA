404ef970a3ada14063dd35ae97791400985786f3
Mukund Patil <mukundpatil5383@gmail.com>
2025-05-28 22:32:33 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (66.44%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
@@ -2,7 +2,10 @@ class Solution {
 public:
     bool checkIfExist(vector<int>& arr) {
         sort(begin(arr),end(arr));
+        // TC = O(Log N + Log M)
+        // Sc = O(1);
 
+        // Approach - First sorting array then using ith index and seraching its arr[i]*2 multiple in the array while i!=mid
         for(int i =0; i<arr.size(); i++){
             int target = arr[i]*2;
 
