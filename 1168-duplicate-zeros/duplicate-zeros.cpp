f54a817de39a880a69c8767954697faa693a8d62
Mukund Patil <mukundpatil5383@gmail.com>
2025-05-28 14:06:34 +0530

    Time: 19 ms (5.37%) | Memory: 14.1 MB (36.86%) - LeetSync

---
+++ 1168-duplicate-zeros/duplicate-zeros.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void duplicateZeros(vector<int>& arr) {
+        int i =0;
+        
+        while(i<arr.size()){
+            if(arr[i]==0){
+                int j = arr.size()-1;
+
+                while(j>i){
+                    if(j==i+1){
+                         arr[j]=0;
+                         i+=1;
+                    }
+                    else{
+                        arr[j]=arr[j-1];
+                    }
+                    j--;
+                }
+            }
+            i++;
+
+        }
+    }
+};
