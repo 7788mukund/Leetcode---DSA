5a85035cdb7cf70bf1c9cd45cb42690133e086e5
Mukund Patil <mukundpatil5383@gmail.com>
2025-05-28 08:41:31 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (34.88%) - LeetSync

--- 66-plus-one/plus-one.cpp
+++ 66-plus-one/plus-one.cpp
@@ -1,37 +1,20 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-            
-
-        if(digits[digits.size()-1]+1 != 10){
-            digits[digits.size()-1] = digits[digits.size()-1]+1;
-            return digits;
-        }
-        else if(digits.size()==1){
-            digits[0] = 1;
-            digits.push_back(0);
-        }
-        else{
-            for(int i = digits.size()-1; i>=0 ; i--){
-                
-                if(digits[i]+1 == 10){
-                 digits[i] = 0;
-                 if(i==0){
-                    digits.insert(digits.begin(),digits[i]+1);
-                 }
-                }
-                else{
-                    digits[i]+=1;
-                    return digits;
-                }
-
+        int i = digits.size()-1;
+        while(i >= 0 ){
+            if(digits[i]+1!=10){
+                digits[i]+=1;
+                return digits;
             }
+            digits[i]=0;
+            if(i==0)
+            {
+                digits.insert(digits.begin(),1);
+                return digits;
+            }
+            i--;
         }
-    
-    
-
-
-
-      return digits;  
+        return digits;
     }
 };
