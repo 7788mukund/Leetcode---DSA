0e7406b02d5469176b02686c607027a220378c78
Mukund Patil <mukundpatil5383@gmail.com>
2025-05-29 22:04:50 +0530

    Time: 0 ms (100.00%) | Memory: 23.3 MB (95.88%) - LeetSync

--- 2524-largest-positive-integer-that-exists-with-its-negative/largest-positive-integer-that-exists-with-its-negative.cpp
+++ 2524-largest-positive-integer-that-exists-with-its-negative/largest-positive-integer-that-exists-with-its-negative.cpp
@@ -1,34 +1,33 @@
 class Solution {
 public:
     int findMaxK(vector<int>& nums) {
+        // firstly we will sort arry so we can apply binary search
+        // Tc = n log n
         sort(nums.begin(), nums.end());
+        // applying binary search its tc log n
+        //Total tc - nlogn+log n = nlog n
 
-        for(int i =0; i<nums.size(); i++){
+        for (int i = 0; i < nums.size(); i++) {
             int target = nums[i];
-            
 
-            int start = i+1; 
-            int end = nums.size()-1;
-            if(nums[nums.size()-1]<0) return -1;
+            int start = i + 1;
+            int end = nums.size() - 1;
+            if (nums[nums.size() - 1] < 0)
+                return -1;
 
-            while(start<=end){
-                int mid = start+(end-start)/2;
-                
-                if(nums[mid]+target==0){
+            while (start <= end) {
+                int mid = start + (end - start) / 2;
+
+                if (nums[mid] + target == 0) {
                     return abs(nums[mid]);
+                } else if (abs(nums[mid]) < abs(target)) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
                 }
-                else if(abs(nums[mid]) < abs(target)){
-                    start = mid+1;
-                }
-                else{
-                    end = mid-1;
-                }
-
             }
-
         }
 
-    return -1;
-        
+        return -1;
     }
 };
