07b62405db7c0d4a328119f5ad53af6080f55a18
Mukund Patil <mukundpatil5383@gmail.com>
2025-05-29 22:00:24 +0530

    Time: 3 ms (79.54%) | Memory: 23.5 MB (81.72%) - LeetSync

--- 2524-largest-positive-integer-that-exists-with-its-negative/largest-positive-integer-that-exists-with-its-negative.cpp
+++ 2524-largest-positive-integer-that-exists-with-its-negative/largest-positive-integer-that-exists-with-its-negative.cpp
@@ -1,37 +1,34 @@
 class Solution {
 public:
     int findMaxK(vector<int>& nums) {
-        // first we will sort array which will take O(n log n) tc
-        // sorting beaucse we can have maximum number at end and its -negtive number at start
-        //fast access
-        sort(begin(nums), end(nums));
-        
-        // using two pointer approach to solve the problem as we need to solve the problem in 
-        //O(n) time complexity for finding the element
+        sort(nums.begin(), nums.end());
 
-        //Total tc = O(n log n )+ o(n) =  O (n log n) 
-        
-        int start = 0;
-        int end = nums.size() - 1;
-        if (nums[end] < 0)
-            return -1;
+        for(int i =0; i<nums.size(); i++){
+            int target = nums[i];
+            
 
-        while (start < end) {
+            int start = i+1; 
+            int end = nums.size()-1;
+            if(nums[nums.size()-1]<0) return -1;
 
-            int numstart = nums[start];
-            int numend = nums[end];
+            while(start<=end){
+                int mid = start+(end-start)/2;
+                
+                if(nums[mid]+target==0){
+                    return abs(nums[mid]);
+                }
+                else if(abs(nums[mid]) < abs(target)){
+                    start = mid+1;
+                }
+                else{
+                    end = mid-1;
+                }
 
-            if (numstart + numend == 0) {
-                return nums[end];
-            } else if (abs(numstart) < abs(numend)) {
-                end--;
-            } else
-                start++;
+            }
+
         }
 
-           return -1;
+    return -1;
+        
     }
 };
-
-// Mistakes - use proper abs double check situation where abs is needed
-// think at edge cases
