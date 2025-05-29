5eb284d4ab9db7389bee2a90cff37d5fc33e26b0
Mukund Patil <mukundpatil5383@gmail.com>
2025-05-29 20:27:18 +0530

    Time: 4 ms (69.63%) | Memory: 23.4 MB (95.88%) - LeetSync

--- 2524-largest-positive-integer-that-exists-with-its-negative/largest-positive-integer-that-exists-with-its-negative.cpp
+++ 2524-largest-positive-integer-that-exists-with-its-negative/largest-positive-integer-that-exists-with-its-negative.cpp
@@ -1,27 +1,34 @@
 class Solution {
 public:
     int findMaxK(vector<int>& nums) {
-      sort(nums.begin(),nums.end());
+        // first we will sort array which will take O(n log n) tc
+        // sorting beaucse we can have maximum number at end and its -negtive number at start
+        //fast access
+        sort(begin(nums), end(nums));
+        
+        // using two pointer approach to solve the problem as we need to solve the problem in 
+        //O(n) time complexity for finding the element
 
-      
-    // sorted the array as largest element at last will come then check for it from front side of array to util we have negative numbers
-    //mistake got error - when all array is negative just check last index after sorting
-    
-    for(int i = nums.size()-1; i>0 ; i--){
-        if(nums[i]<0) return -1;
-        int maximum = nums[i];
-        int j =0;
+        //Total tc = O(n log n )+ o(n) =  O (n log n) 
+        
+        int start = 0;
+        int end = nums.size() - 1;
+        if (nums[end] < 0)
+            return -1;
 
-        while(nums[j]<0){
-          if(maximum == abs(nums[j]))
-            return maximum;
-           else 
-            j++;
+        while (start < end) {
+
+            int numstart = nums[start];
+            int numend = nums[end];
+
+            if (numstart + numend == 0) {
+                return nums[end];
+            } else if (abs(numstart) < abs(numend)) {
+                end--;
+            } else
+                start++;
         }
 
-    }
-
-      return -1;
-        
+           return -1;
     }
 };
