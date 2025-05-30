fc8102757413b12532bacf5f1bbeb804eefa6a45
Mukund Patil <mukundpatil5383@gmail.com>
2025-05-30 14:09:27 +0530

    Time: 0 ms (100.00%) | Memory: 23.4 MB (85.60%) - LeetSync

--- 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.cpp
+++ 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     string firstPalindrome(vector<string>& words) {
+        // Tc - N+n/2  (for loop words and while loop i and j)
+        // Sc = O(4) = O(1)
+
+        // approach = firstly extracting the word from the vector string example 
+        // then comparing the extracted word from front and back if not equal then return false for current word increment i and j
+
         
         string answer ="";
         for(auto &word: words){
@@ -8,7 +14,8 @@ public:
             int i =0; int j = word.size()-1;
             while(i<j){
                 if(word[i]!=word[j]){
-                    check = false;
+                    check = false; 
+                    break;
                 }
                 i++;
                 j--;
