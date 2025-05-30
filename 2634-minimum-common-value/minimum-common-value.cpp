fded8fbbbd2b5acda367dcf550c7107de0e60739
Mukund Patil <mukundpatil5383@gmail.com>
2025-05-30 13:50:49 +0530

    Time: 0 ms (100.00%) | Memory: 54.7 MB (30.25%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.cpp
+++ 2634-minimum-common-value/minimum-common-value.cpp
@@ -2,6 +2,13 @@ class Solution {
 public:
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
         int i =0; int j=0;
+    // Tc - O(n+m) Sc - O(1)
+    // we will use while loop to iterate the arrays 
+    // as array is sorted so it becomes easy solve the problem
+    // approach - as the if the nums1 element is less than nums2 element we increse ptr nums1
+    // and vice versa
+    // why so beause if we want target to achive we need to move grater value which is minimum but equal in both
+
 
         while(i<nums1.size() && j < nums2.size()){
             if(nums1[i]==nums2[j]){
