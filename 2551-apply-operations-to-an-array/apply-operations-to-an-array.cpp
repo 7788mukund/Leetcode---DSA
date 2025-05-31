ab6ed485bf8db32cecff064ee43e55fba1ef5065
Mukund Patil <mukundpatil5383@gmail.com>
2025-05-31 12:32:15 +0530

    Time: 0 ms (100.00%) | Memory: 13.1 MB (86.63%) - LeetSync

--- 2551-apply-operations-to-an-array/apply-operations-to-an-array.cpp
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.cpp
@@ -1,15 +1,7 @@
 class Solution {
 public:
     vector<int> applyOperations(vector<int>& nums) {
-        //Used extra space as question wanted to return resulting as vector
-        // SC - O(n)
-        vector<int> result(nums.size(),0);
-        //Tc 1st loop - O(n) and 2nd loop O(n) 
-        //Total Tc - O(n);
-
-        // Approach made all element 0 in result as we dont need to shift 0 at back
-        // then we moved one loop to perfrom operation acording to condition
-        // then we added all non zero value to result vector
+        
         for(int i =0; i<nums.size()-1; i++){
             if(nums[i]==nums[i+1]){
 
@@ -18,15 +10,29 @@ public:
         
             }
         }
-        
-        int j =0;
-        for(int i =0; i<nums.size(); i++){
-            if(nums[i]!=0){
-                result[j] = nums[i];
-                j++;
+
+        int first =0; int second = 1;
+
+        while(second<nums.size()){
+            if(nums[first]==0 && nums[second]!=0){
+                swap(nums[first], nums[second]);
+                first++;
+                second++;
+            }
+            else if(nums[first]!=0 && nums[second]==0){
+                first++; second++;
+            }
+            else if(nums[first]!=0 && nums[second]!=0){
+                first++;
+                second++;
+            }
+            else{
+                second++;
             }
         }
+        
+     
 
-      return result;
+      return nums;
     }
 };
