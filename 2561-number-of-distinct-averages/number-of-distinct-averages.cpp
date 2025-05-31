51af9cf4ad28177afd42ce4590996312dcece8b7
Mukund Patil <mukundpatil5383@gmail.com>
2025-05-31 14:28:44 +0530

    Time: 3 ms (6.57%) | Memory: 9.8 MB (17.78%) - LeetSync

--- 2561-number-of-distinct-averages/number-of-distinct-averages.cpp
+++ 2561-number-of-distinct-averages/number-of-distinct-averages.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     int distinctAverages(vector<int>& nums) {
+        // Tc = N log N + O(N) = N log N
+        // Sc = O(N) extra space 
+        // sort the nums
+        // make loop using two pointer front and back 
+        // insert element into set so no duplicate remain 
+        // return numset size meaning no of unique element
         sort(nums.begin(),nums.end());
         unordered_set<double> numset;
         int start =0; int end = nums.size()-1;
