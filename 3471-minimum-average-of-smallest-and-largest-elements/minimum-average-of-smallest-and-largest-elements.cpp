5e5b21e50c6d884320d744b721d72428e64d9bd7
Mukund Patil <mukundpatil5383@gmail.com>
2025-05-31 13:21:29 +0530

    Time: 0 ms (100.00%) | Memory: 25.8 MB (38.74%) - LeetSync

--- 3471-minimum-average-of-smallest-and-largest-elements/minimum-average-of-smallest-and-largest-elements.cpp
+++ 3471-minimum-average-of-smallest-and-largest-elements/minimum-average-of-smallest-and-largest-elements.cpp
@@ -3,29 +3,30 @@ public:
     double minimumAverage(vector<int>& nums) {
 
         // Sc = O(n)
-        // Tc = NlogN + O(N) + NlogN = N log N 
+        // Tc = NlogN + O(N) + NlogN = N log N
 
-        //Aproach = sort nums so larger and min element comes 1 and n-1
-        // take two pointer at start and end to calculate the avg of them and store in avg array
-        //then sort avg so we can get the minimum element at 0th index;
-        
+        // Aproach = sort nums so larger and min element comes 1 and n-1
+        //  take two pointer at start and end to calculate the avg of them and
+        //  store in avg array
+        // then sort avg so we can get the minimum element at 0th index;
 
         vector<double> averages;
 
-        sort(begin(nums),end(nums));
+        sort(begin(nums), end(nums));
 
-        int start =0; int end = nums.size()-1;
+        int start = 0;
+        int end = nums.size() - 1;
 
-        while(start<end){
-            double avg  = (nums[start]+nums[end])/2.0;
+        while (start < end) {
+            double avg = (nums[start] + nums[end]) / 2.0;
 
             averages.push_back(avg);
             start++;
             end--;
         }
 
-        sort(averages.begin(),averages.end());
-        
+        sort(averages.begin(), averages.end());
+
         return averages[0];
     }
 };
