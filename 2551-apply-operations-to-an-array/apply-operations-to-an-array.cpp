a02210d10e27162cd1bcbec8e3a046682c872c01
Mukund Patil <mukundpatil5383@gmail.com>
2025-05-31 12:13:51 +0530

    Time: 3 ms (9.75%) | Memory: 13.1 MB (59.94%) - LeetSync

--- 2551-apply-operations-to-an-array/apply-operations-to-an-array.cpp
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.cpp
@@ -1,8 +1,15 @@
 class Solution {
 public:
     vector<int> applyOperations(vector<int>& nums) {
+        //Used extra space as question wanted to return resulting as vector
+        // SC - O(n)
         vector<int> result(nums.size(),0);
+        //Tc 1st loop - O(n) and 2nd loop O(n) 
+        //Total Tc - O(n);
 
+        // Approach made all element 0 in result as we dont need to shift 0 at back
+        // then we moved one loop to perfrom operation acording to condition
+        // then we added all non zero value to result vector
         for(int i =0; i<nums.size()-1; i++){
             if(nums[i]==nums[i+1]){
 
