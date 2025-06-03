1122939581fc2f844480d2257264feab3267695a
Mukund Patil <mukundpatil5383@gmail.com>
2025-06-03 10:29:27 +0530

    Time: 0 ms (100.00%) | Memory: 25.8 MB (76.94%) - LeetSync

--- 958-sort-array-by-parity-ii/sort-array-by-parity-ii.cpp
+++ 958-sort-array-by-parity-ii/sort-array-by-parity-ii.cpp
@@ -7,27 +7,32 @@ class Solution {
         else
             return false;
     }
-    public:
-        vector<int> sortArrayByParityII(vector<int> & nums) {
 
-            // Tc = O(N)
-            //Sc = O(1)
-            //Approach - as we are asked to check of even and odd index and add according values at their even and odd places
-            // So the solution can be thought as we will check current index 
-            //if index is value is even then we will skip it to next+1 index to skip odd index
-            // same for odd index
-            // out loop will run until evenindex and odd index are less than nums size
-            
-            int evenindex = 0;
-            int oddindex = 1;
-            while(evenindex<nums.size() && oddindex<nums.size()){
-                if(Iseven(nums,evenindex)) evenindex+=2;
-                else if(!Iseven(nums,oddindex)) oddindex+=2;
-                else{
-                    swap(nums[evenindex],nums[oddindex]);
-                }
+public:
+    vector<int> sortArrayByParityII(vector<int>& nums) {
+
+        // Tc = O(N)
+        // Sc = O(1)
+        // Approach - as we are asked to check of even and odd index and add
+        // according values at their even and odd places
+        // So the solution can be thought as we will check current index
+        // if index is value is even then we will skip it to next+1 index to
+        // skip odd index
+        // same for odd index
+        // out loop will run until evenindex and odd index are less than nums
+        // size
+        int evenindex = 0;
+        int oddindex = 1;
+        while (evenindex < nums.size() && oddindex < nums.size()) {
+            if (Iseven(nums, evenindex))
+                evenindex += 2;
+            else if (!Iseven(nums, oddindex))
+                oddindex += 2;
+            else {
+                swap(nums[evenindex], nums[oddindex]);
             }
-           
-            return nums;
         }
-    };
+
+        return nums;
+    }
+};
