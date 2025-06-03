c44d9bedfd3f84602da20b5bee7c87c28d0311a8
Mukund Patil <mukundpatil5383@gmail.com>
2025-06-03 09:23:33 +0530

    Time: 0 ms (100.00%) | Memory: 20.4 MB (90.14%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.cpp
+++ 941-sort-array-by-parity/sort-array-by-parity.cpp
@@ -1,10 +1,20 @@
 class Solution {
+    // Function tells us whether the index is even or odd
+    // return true or false
     bool Iseven(vector<int> &nums, int index){
         if(nums[index]%2==0) return true;
         else return false;
     }
 public:
     vector<int> sortArrayByParity(vector<int>& nums) {
+        // Tc = O(N) , Sc = O(1);
+        // Approach 
+        // as we know we just need to shift even number at front rest odd at back
+        // so we can us two pointer concept evenindex ptr and evenindexptr+1;
+        // writing down condition if we got the index as even then increse ptr
+        // if both index element are even then incrementing both
+        // if evenptr found odd and ptr found even then swap them
+        // as result array got in term as asked in question
         if(nums.size()==1) return nums;
 
         int evenptr = 0;
