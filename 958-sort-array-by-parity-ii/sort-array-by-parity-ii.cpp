6b47c668ce0e6ce1e827b8a050f3517536d3b021
Mukund Patil <mukundpatil5383@gmail.com>
2025-06-03 10:29:07 +0530

    Time: 4 ms (11.61%) | Memory: 26 MB (25.85%) - LeetSync

--- 958-sort-array-by-parity-ii/sort-array-by-parity-ii.cpp
+++ 958-sort-array-by-parity-ii/sort-array-by-parity-ii.cpp
@@ -1,40 +1,33 @@
 class Solution {
-public:
-    vector<int> sortArrayByParityII(vector<int>& nums) {
-        vector<int> evennum;
-        vector<int> oddnum;
-
-// Tc = O(N)+O(N)+O(N) = O(N)
-// Space complexity = (N)
-// Taking two arrays of even num and odd num  name
-//storing of even values to evennum and odd values oddnum 
-// as we have evena and odd then iterating trough num  at  i = 0 to i+2 positions
-//inserting all even and odd values from the evenum and odd num to nums 
-// returning it
-        for(int num :nums){
-            if(num%2==0) {
-               evennum.push_back(num);
-                
-            }else{
-                oddnum.push_back(num);
-
-            }
-            
-        }
-
-        int ptreven = 0;
-       for(int even : evennum){
-        nums[ptreven] = even;
-        ptreven+=2;
-       }
-
-       int ptrodd = 1;
-       for(int odd : oddnum){
-        nums[ptrodd] = odd;
-        ptrodd+=2;
-       }
-
-
-    return nums;
+    // Function tells us whether the index is even or odd
+    // return true or false
+    bool Iseven(vector<int>& nums, int index) {
+        if (nums[index] % 2 == 0)
+            return true;
+        else
+            return false;
     }
-};
+    public:
+        vector<int> sortArrayByParityII(vector<int> & nums) {
+
+            // Tc = O(N)
+            //Sc = O(1)
+            //Approach - as we are asked to check of even and odd index and add according values at their even and odd places
+            // So the solution can be thought as we will check current index 
+            //if index is value is even then we will skip it to next+1 index to skip odd index
+            // same for odd index
+            // out loop will run until evenindex and odd index are less than nums size
+            
+            int evenindex = 0;
+            int oddindex = 1;
+            while(evenindex<nums.size() && oddindex<nums.size()){
+                if(Iseven(nums,evenindex)) evenindex+=2;
+                else if(!Iseven(nums,oddindex)) oddindex+=2;
+                else{
+                    swap(nums[evenindex],nums[oddindex]);
+                }
+            }
+           
+            return nums;
+        }
+    };
