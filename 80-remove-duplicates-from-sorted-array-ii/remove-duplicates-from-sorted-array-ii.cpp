8c6a926b9714f89193450323d44db3cb64dc75ce
Mukund Patil <mukundpatil5383@gmail.com>
2025-06-05 09:25:11 +0530

    Time: 4 ms (84.45%) | Memory: 19.3 MB (95.27%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
@@ -1,12 +1,18 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
+        //Tc = O(N)
+        //Sc = O(1)
+
+        // Edge case handling
         if(nums.size()==1) return 1;
 
         int currentindex = 2;
         int IndexToUpdate = 2;
 
-        
+        // Approach - This thhis question of  two pointer as array is sorted and need to remove element occuring more than twice 
+        // slow and fast pointer technique
+        // Watched the solution will come back again to solve it
 
         while (currentindex < nums.size()) {
             if (nums[currentindex] == nums[IndexToUpdate - 2]) {
