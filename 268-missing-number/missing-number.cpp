503440075e993829b4370a648129d9c552d8556c
Mukund Patil <mukundpatil5383@gmail.com>
2025-06-05 13:14:07 +0530

    Time: 1 ms (24.31%) | Memory: 22.5 MB (7.61%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,12 +1,19 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        vector<int> num(nums.size()+1,-1);
+        // Tc = O(N)
+        //Sc = O(N)
 
+
+        // Take extra space array of nums+1 size initalisze all to -1
+        vector<int> num(nums.size()+1,-1);
+    // then find out index element and change the value at extra taken array by one
         for(int i =0; i<nums.size(); i++){
             num[nums[i]] +=1;
         }
 
+
+        // search whweere is the -1 is present return that index
         for(int i =0; i<num.size(); i++)
         {
             if(num[i]==-1){
