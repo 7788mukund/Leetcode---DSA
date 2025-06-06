80b1439b86a2d5391ce931ee8fdc3b4656fbe237
Mukund Patil <mukundpatil5383@gmail.com>
2025-06-06 11:10:08 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (94.25%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
@@ -1,4 +1,5 @@
 class Solution {
+    // Function to check wether the current letter is vowel or not
      bool isVowel(char ch ){
      return (ch=='a'||ch=='A'||
         ch=='e'||ch=='E'||
@@ -13,7 +14,15 @@ public:
 
          int j =s.size()-1;
          int i =0;
+    //using two pointer approach to reverse the vowels
+    // TC = O(N)
+    //Sc = O(1)
 
+    // why is works - beaucse we know string has vowels from both side start and end
+    // so as we need to reverse it so we will travese from front and back
+    //whenever i index char and j index char is vowels we will reveerse them
+    // else if the i is not vowels so increse its index
+    // else decrese j index so that both come to right vovwel conatining index
         while(i<j){
            if(!isVowel(s[i])){
                 i++;
