0410717137754e1a7d159bfc100a5c418b3a31bf
Mukund Patil <mukundpatil5383@gmail.com>
2025-06-07 08:36:32 +0530

    Time: 0 ms (100.00%) | Memory: 14.4 MB (98.01%) - LeetSync

--- 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.cpp
+++ 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.cpp
@@ -1,19 +1,35 @@
 class Solution {
 public:
     bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
-        
-       string  word1merged = "";
-       string word2merged ="";
+        int pointerword1 = 0;
+        int indexword1 = 0;
 
-       for(string word : word1){
-         word1merged+= word;
-       }
-       for(string word : word2){
-         word2merged+= word;
-       }
+        int pointerword2 = 0;
+        int indexword2 = 0;
 
-        if(word1merged==word2merged) return true;
+        while (pointerword1 < word1.size() && pointerword2 < word2.size()) {
 
-        return false;
+            char word1char = word1[pointerword1][indexword1];
+            char word2char = word2[pointerword2][indexword2];
+
+            if (word1char != word2char)
+                return false;
+
+            indexword1++;
+            indexword2++;
+
+            if (indexword1 == word1[pointerword1].size()) {
+                indexword1 = 0;
+                pointerword1++;
+            }
+
+            if (indexword2 == word2[pointerword2].size()) {
+                indexword2 = 0;
+                pointerword2++;
+            }
+
+        }
+        return (pointerword1 == word1.size() && pointerword2 == word2.size());
+             
     }
 };
