24e965ad72f8494bdad77e588925d2b4b3a3b68f
Mukund Patil <mukundpatil5383@gmail.com>
2025-06-07 12:32:51 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (76.81%) - LeetSync

--- 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.cpp
+++ 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.cpp
@@ -1,31 +1,37 @@
 class Solution {
+    //Function to reverse the string before ' ' space
+    void reversestring(string& s, int startindex, int endindex) {
+        while (startindex < endindex) {
+            swap(s[startindex], s[endindex]);
+            startindex++;
+            endindex--;
+        }
+    }
+
 public:
     string reverseWords(string s) {
+        // Using two pointer we can reverse the string from the point we find '' white space 
+        // its very simple if we got '' space at ith index we will skip it
+        // if got white space at jth index we will take j-1 index and i index
+        // reverse the chars from ith index to j -1 index 
+        // hence our string will be reverse using words reverse
+        
 
         int i = 0;
         int j = i + 1;
 
-        while(i<s.size()){
-            
-            if(s[i]==' '){
+        while (i < s.size()) {
+
+            if (s[i] == ' ') {
                 i++;
                 j++;
                 continue;
-            }
-            else if(j<s.size()&&s[j]!=' '){
+            } else if (j < s.size() && s[j] != ' ') {
                 j++;
-            }
-            else{
-                int start = i;
-                int end = j-1;
-
-                while(start<end){
-                    swap(s[start],s[end]);
-                    start++;
-                    end--;
-                }
-                i = j+1;
-                j = i+1;
+            } else {
+                reversestring(s, i, j - 1);
+                i = j + 1;
+                j = i + 1;
             }
         }
         return s;
