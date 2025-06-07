1457508e9af67dd3a7f819c0cdbfe0fc346fd9b6
Mukund Patil <mukundpatil5383@gmail.com>
2025-06-07 10:36:07 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (81.41%) - LeetSync

---
+++ 742-to-lower-case/to-lower-case.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string toLowerCase(string s) {
+        
+        for(int i =0; i<s.size(); i++){
+
+            if(!islower(s[i])){
+                s[i]= tolower(s[i]);
+            }
+
+        }
+
+        return s;
+    }
+};
