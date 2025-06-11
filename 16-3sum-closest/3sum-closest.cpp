d21421cb9366e784ea7a130712e63b73daf58636
Mukund Patil <mukundpatil5383@gmail.com>
2025-06-11 09:04:22 +0530

    Time: 6 ms (90.82%) | Memory: 14 MB (61.94%) - LeetSync

--- 16-3sum-closest/3sum-closest.cpp
+++ 16-3sum-closest/3sum-closest.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
 
-
+        // TC = O(N) Sc - O(1)
         sort(nums.begin(),nums.end());
 
         int result = nums[0]+nums[1]+nums[2];
