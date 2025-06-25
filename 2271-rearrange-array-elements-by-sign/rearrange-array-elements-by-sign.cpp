0748d3bb8070402224a89423845d701b3b5bc32a
Mukund Patil <mukundpatil5383@gmail.com>
2025-06-25 13:18:18 +0530

    Time: 7 ms (60.37%) | Memory: 128.1 MB (94.77%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -1,40 +1,23 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        vector<int> posnegnum;
 
-        // Storing first positive number in begin of temparr
+        vector<int> result(nums.size());
+
+        int posptr = 0;
+        int negptr = 1;
+
         for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] > 0) {
-                posnegnum.push_back(nums[i]);
-            }
-        }
-         
-         //storing negative number in temparr after positive
-        for (int i = 0; i < nums.size(); i++) {
+
             if (nums[i] < 0) {
-                posnegnum.push_back(nums[i]);
+                result[negptr] = nums[i];
+                negptr+=2;
+            } else {
+                result[posptr] = nums[i];
+                posptr+=2;
             }
         }
 
-        int ptr = 0;
-        // putting positive number at indexs  its to be required
-        // 0 , 2 , 4 indexs as even indexes
-        for (int i = 0; i < nums.size(); i += 2) {
-
-            nums[i] = posnegnum[ptr];
-            ptr++;
-        }
-
-        // putting negative number at required odd indexes
-        // eg indexes = 1,3,5
-
-        for (int i = 1; i < nums.size(); i += 2) {
-
-            nums[i] = posnegnum[ptr];
-            ptr++;
-        }
-
-        return nums;
+        return result;
     }
 };
