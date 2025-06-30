c02e8ac733efc53b8ba5c72e273da3cc742753bb
Mukund Patil <mukundpatil5383@gmail.com>
2025-06-30 23:07:08 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (78.01%) - LeetSync

--- 389-find-the-difference/find-the-difference.cpp
+++ 389-find-the-difference/find-the-difference.cpp
@@ -2,25 +2,20 @@ class Solution {
 public:
     char findTheDifference(string s, string t) {
 
+        sort(begin(t), end(t));
+        sort(begin(s), end(s));
 
-            sort(begin(t),end(t));
-             sort(begin(s),end(s));
+        int ptrs = 0, ptrt = 0;
 
+        while (ptrs < s.size() && ptrt < t.size()) {
 
-            int ptrs = 0, ptrt = 0;
-
-            while(ptrs < s.size() && ptrt < t.size()){
-
-
-                if(s[ptrs] == t[ptrt]){
-                    ptrs++;
-                    ptrt++;
-                }
-                else{
-                    ptrs++;
-                }
-
+            if (s[ptrs] == t[ptrt]) {
+                ptrs++;
+                ptrt++;
+            } else {
+                ptrs++;
             }
+        }
 
         return t[ptrt];
     }
