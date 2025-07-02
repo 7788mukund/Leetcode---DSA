1c12876c00f89bed3693b5928800b3746d93d007
Mukund Patil <mukundpatil5383@gmail.com>
2025-07-02 18:49:12 +0530

    Time: 5 ms (74.03%) | Memory: 67.3 MB (51.08%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
@@ -1,30 +1,53 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-        int arr[] = new int[nums.length];
 
-        int ptr = 0;
+        int lesscnt = 0, equalcnt = 0, greatercnt = 0;
+
         for (int i = 0; i < nums.length; i++) {
+
+            if (nums[i] < pivot)
+                lesscnt++;
+            else if (nums[i] > pivot)
+                greatercnt++;
+            else
+                equalcnt++;
+
+        }
+
+        int lessarr[] = new int[lesscnt];
+        int equalarr[] = new int[equalcnt];
+        int greaterarr[] = new int[greatercnt];
+        int lessptr = 0, equalptr = 0, greaterptr = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
             if (nums[i] < pivot) {
-                arr[ptr] = nums[i];
-                ptr++;
+                lessarr[lessptr] = nums[i];
+                lessptr++;
+            } else if (nums[i] == pivot) {
+                equalarr[equalptr] = nums[i];
+                equalptr++;
+            } else {
+                greaterarr[greaterptr] = nums[i];
+                greaterptr++;
             }
         }
 
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == pivot) {
-                arr[ptr] = nums[i];
-                ptr++;
-            }
+        int resultarr[] = new int[nums.length];
+
+        int resultindex = 0;
+
+        for(int i = 0; i<lessarr.length; i++){
+            resultarr[resultindex++]=lessarr[i];
+        }
+        for(int i = 0; i<equalarr.length; i++){
+            resultarr[resultindex++]=equalarr[i];
+        }
+        for(int i = 0; i<greaterarr.length; i++){
+            resultarr[resultindex++]=greaterarr[i];
         }
 
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] > pivot) {
-                arr[ptr] = nums[i];
-                ptr++;
-            }
-
-        }
-
-        return arr;
+    
+    return resultarr;
     }
 }
