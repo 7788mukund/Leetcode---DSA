0dc50a6bf37af3210361c5dc31d700eed8326494
Mukund Patil <mukundpatil5383@gmail.com>
2025-07-03 22:59:28 +0530

    Time: 8 ms (31.98%) | Memory: 41.1 MB (41.51%) - LeetSync

---
+++ 441-arranging-coins/arranging-coins.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int arrangeCoins(int n) {
+
+        int count = 0;
+        int row = 1;
+
+        while (n - row >= 0) {
+            n -= row;
+            row++;
+            count++;
+        }
+
+        return count;
+
+    }
+}
